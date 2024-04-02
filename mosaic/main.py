0b8f619a626f96d7f4d0c316d0202df529d790d4
Johan Rönnkvist <johan.ronnkvist@gmail.com>
2024-04-02 19:05:56 +0200

    feat: added inspect dockwidget

--- mosaic/main.py
+++ mosaic/main.py
@@ -4,23 +4,8 @@ import sys
 import mosaic.widgets as widgets
 from mosaic import config
 from mosaic.application import MosaicEditor
-from mosaic.core.builder import LazyInit
-from mosaic.domain.tilescene import TileScene
-from mosaic.widgets.main_menu import MainMenu
-from mosaic.widgets.main_window import MainWindow
-from mosaic.widgets.scene_view import SceneView
-from mosaic.widgets.status_bar import StatusBar
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-def register_widgets():
-    factory = widgets.factory
-    factory.register(MainMenu, instance=LazyInit)
-    factory.register(StatusBar, instance=LazyInit)
-    factory.register(MainWindow, instance=LazyInit)
-    factory.register(SceneView)
-    factory.register(TileScene, instance=TileScene())
 
 
 def main() -> int:
--- mosaic/widgets/main_window.py
+++ mosaic/widgets/main_window.py
@@ -1,20 +1,33 @@
+import logging
+
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QMainWindow
+from qtawesome import icon
 
 from mosaic import actions
+from mosaic.widgets.graphics_view import GraphicsView
+from mosaic.widgets.inspector import InspectDockable
 from mosaic.widgets.main_menu import MainMenu
 from mosaic.widgets.status_bar import StatusBar
 
+_logger = logging.getLogger(__name__)
+
 
 class MainWindow(QMainWindow):
-    def __init__(self, main_menu: MainMenu, status_bar: StatusBar, parent=None):
+    def __init__(
+        self, main_menu: MainMenu, status_bar: StatusBar, view: GraphicsView, inspector: InspectDockable, parent=None
+    ):
         super().__init__(parent)
         self.setMinimumSize(800, 600)
+        self.setWindowIcon(icon("fa5s.image"))
 
         self.setMenuBar(main_menu)
         self.setStatusBar(status_bar)
-        # self.setCentralWidget(view)
+        self.setCentralWidget(view)
 
         self.populate_menus()
+
+        self.addDockWidget(Qt.LeftDockWidgetArea, inspector)
 
     def populate_menus(self):
         menu = self.menuBar()
